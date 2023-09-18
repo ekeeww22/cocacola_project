0e4a517e0c347595e2bb15757e0f36f5b2b8ba45
eunha <gangssu0@gmail.com>
2023-09-18 17:55:27 +0900

    f

--- script/common.js
+++ script/common.js
@@ -3,18 +3,19 @@
 const header = document.querySelector('header')
 
 /* 슬라이드 했을 때 header 색상 변경 */
-/* window.addEventListener('scroll',function(){
+window.addEventListener('scroll',function(){
     if(this.window.scrollY > 100){
         header.style.backgroundColor = '#bf0000'
     }else{
         header.style.backgroundColor = 'rgba(255, 255, 255, 0.00)'
     }
-}) */
+})
 
 /* quick menu - top btn */
 const topBtn = document.querySelector('.top_btn')
 console.log(topBtn)
-topBtn.addEventListener('click',function(){
+topBtn.addEventListener('click',function(e){
+    e.preventDefault()
     window.scrollTo({left:0, top:0})
 })
 /* m_nav */
@@ -24,12 +25,14 @@ const mNav = document.querySelector('.m_open_nav')
 console.log(closeBtn, openBtn, mNav)
 mNav.style.display = 'none'
 closeBtn.style.display = 'none'
-openBtn.addEventListener('click',function(){
+openBtn.addEventListener('click',function(e){
+    e.preventDefault()
     openBtn.style.display = 'none'
     closeBtn.style.display = 'flex'
     mNav.style.display = 'block'
 })
-closeBtn.addEventListener('click',function(){
+closeBtn.addEventListener('click',function(e){
+    e.preventDefault()
     closeBtn.style.display = 'none'
     openBtn.style.display = 'flex'
     mNav.style.display = 'none'
