eff7b9dfa805d110d2cffaa3ad1bf742a1eb259a
eunha <gangssu0@gmail.com>
2023-09-18 12:59:35 +0900

    f

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer>
+    <div class="top">
+        <a href="#"><img src="./images/logo_w.png" alt="cocacola korea"></a>
+        <div class="sns">
+            <a href="#"><img src="./images/youtube_circle.png" alt="youtube"></a>
+            <a href="#"><img src="./images/twitter_circle.png" alt="twitter"></a>
+            <a href="#"><img src="./images/instagram_circle.png" alt="instagram"></a>
+            <a href="#"><img src="./images/facebook_circle.png" alt="facebook"></a>
+            <a href="#"><img src="./images/cokeplay_circle.png" alt="cokeplay"></a>
+        </div>
+        
+    </div>
+    <div class="btm">
+        <span>2022 The Coca-Cola Company. All Rights Reserved.</span>
+        <div class="info">
+            <a href="#">고객 안내</a>
+            <a href="#">사이트 운영정책</a>
+            <a href="#">히스토리</a>
+            <a href="#">글로벌 코카-콜라</a>
+        </div>
+        
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,43 @@
+<header>
+    <h1><a href="./index.html"><img src="./images/logo_w.png" alt="cocacola korea"></a></h1>
+    <!-- 태블릿, 데스크탑 메인메뉴 -->
+    <nav>
+        <ul class="nav_menu">
+            <li><a href="./company.html">Our Company</a></li>
+            <li><a href="./brands.html">Brands</a></li>
+            <li><a href="./business.html">Sustainable Business</a></li>
+            <li><a href="./stories.html">Stories</a></li>
+            <li><a href="#">Creations</a></li>
+            <li><a href="#">Now</a></li>
+        </ul>
+        <ul class="nav_right">
+            <li><a href="#"><img src="./images/icon/search2_w.png" alt="검색"></a></li>
+            <li><a href="#"><img src="./images/icon/internet_w.png" alt="글로벌 코카콜라 홈페이지"></a></li>
+        </ul>
+        <!-- 태블릿, 데스크탑 메인메뉴 -->
+        <div class="m_nav_btn">
+            <a href="#" class="open_btn">
+                <span></span><span></span><span></span>
+            </a>
+            <a href="#" class="close_btn">
+                <span></span><span></span><span></span>
+            </a>
+        </div>
+    </nav>
+    <!-- 모바일 메인메뉴 -->
+    <div class="m_open_nav">
+        <button type="button" id="close" title="모바일 메뉴 닫기"></button>
+        <ul class="nav_menu">
+            <li><a href="./company.html">Our Company</a></li>
+            <li><a href="./brands.html">Brands</a></li>
+            <li><a href="./business.html">Sustainable Business</a></li>
+            <li><a href="./stories.html">Stories</a></li>
+            <li><a href="#">Creations</a></li>
+            <li><a href="#">Now</a></li>
+        </ul>
+        <ul class="nav_right">
+            <li><a href="#"><img src="./images/icon/search2.png" alt="검색"></a></li>
+            <li><a href="#"><img src="./images/icon/internet_b.png" alt="글로벌 코카콜라 홈페이지"></a></li>
+        </ul>
+    </div>
+</header>
